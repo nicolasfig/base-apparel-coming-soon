d303608f583f3ca045aded1b633320a420d360ff
Nicolas Figueroa <nicolasfigb@gmail.com>
2023-01-12 19:15:48 -0500

    Project base structure

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    screens: {
+      desktop: "765px",
+    },
+    fontFamily: {
+      josefin: ["Josefin Sans", "sans-serif"],
+    },
+    fontWeight: {
+      light: 300,
+      regular: 400,
+      semibold: 600,
+    },
+    extend: {
+      colors: {
+        "desaturated-red": "hsl(0, 36%, 70%)",
+        "soft-red": "hsl(0, 93%, 68%)",
+        "dark-grayish-red": "hsl(0, 6%, 24%)",
+      },
+      backgroundImage: {
+        "hero-mobile": "url(./images/hero-mobile.jpg)",
+        "hero-desktop": "url(./images/hero-desktop.jpg)",
+      },
+    },
+  },
+  plugins: [],
+};
