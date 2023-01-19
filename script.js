b9d8b12166b2163dbc91c52fdafa590067ddd49a
Nicolas Figueroa <nicolasfigb@gmail.com>
2023-01-18 20:56:38 -0500

    Added script

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const input = document.querySelector(".form__input");
 const button = document.querySelector(".form__button");
+
+button.addEventListener("click", () => {
+  console.log("clicked");
+});
