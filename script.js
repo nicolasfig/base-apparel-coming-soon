ac28a7536ff93fab2950288e597db7a21dedfb95
Nicolas Figueroa <nicolasfigb@gmail.com>
2023-01-20 22:04:36 -0500

    Updated script

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const input = document.querySelector(".form__input");
 const button = document.querySelector(".form__button");
+const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 button.addEventListener("click", () => {
   console.log("clicked");
 });
+
+const validation = () => {
+  if (input.value === "") {
+    console.log("Email cannot be empty");
+  } else {
+    input.innerHTML = "";
+  }
+  if (!pattern.test(input.value)) {
+    console.log("Looks like this is not an email");
+  } else {
+    input.innerHTML = "";
+  }
+};
