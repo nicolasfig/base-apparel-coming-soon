58514a8af3665718cb4171643f94875245e93fb2
Nicolas Figueroa <nicolasfigb@gmail.com>
2023-01-21 21:34:23 -0500

    Testing validation function

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ const input = document.querySelector(".form__input");
 const button = document.querySelector(".form__button");
 const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
-button.addEventListener("click", () => {
-  console.log("clicked");
-});
-
 const validation = () => {
   if (input.value === "") {
     console.log("Email cannot be empty");
-  } else {
-    input.innerHTML = "";
-  }
-  if (!pattern.test(input.value)) {
+    input.placeholder = "";
+    input.value = "Email cannot be empty";
+  } else if (!pattern.test(input.value)) {
     console.log("Looks like this is not an email");
+    input.placeholder = "";
+    input.value = "Looks like this is not an email";
   } else {
     input.innerHTML = "";
   }
 };
+
+button.addEventListener("click", () => {
+  validation();
+});
