094c300e68435c0b4a02702183ed919129d1475a
Nicolas Figueroa <nicolasfigb@gmail.com>
2023-01-24 21:34:45 -0500

    Error message modifications

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 const validation = () => {
   if (input.value === "") {
-    input.placeholder = "Email cannot be empty";
+    input.placeholder = "";
     error.classList.toggle("hidden");
     error.innerHTML = "Email cannot be empty";
   } else if (!pattern.test(input.value)) {
-    console.log("Looks like this is not an email");
     input.placeholder = "";
     error.classList.toggle("hidden");
     error.innerHTML = "Looks like this is not an email";
